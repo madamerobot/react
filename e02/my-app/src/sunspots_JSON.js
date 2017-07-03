138d1de3d9001250160c8e9ed8439351b80d13fa
Valerie Fuchs <hi@valerie-fuchs.de>
2017-07-03 10:08:49 +0200

    Added second app skeleton

---
+++ e02/my-app/src/sunspots_JSON.js
@@ -0,0 +1,545 @@
+var DATA = [
+
+    {
+        "geometry": {
+            "coordinates": [
+                5.019545,
+                52.354732
+            ],
+            "type": "Point"
+        },
+        "id": 1,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Blijburg"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.862764,
+                52.357631
+            ],
+            "type": "Point"
+        },
+        "id": 2,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Vondelpark"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.899946,
+                52.360008
+            ],
+            "type": "Point"
+        },
+        "id": 3,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Frederiksplein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.883911,
+                52.384724
+            ],
+            "type": "Point"
+        },
+        "id": 4,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Haarlemmerplein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.879872,
+                52.394321
+            ],
+            "type": "Point"
+        },
+        "id": 5,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Strand West"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.892446,
+                52.398936
+            ],
+            "type": "Point"
+        },
+        "id": 6,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Pllek"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.926724,
+                52.372265
+            ],
+            "type": "Point"
+        },
+        "id": 10,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Roest"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.886341,
+                52.341121
+            ],
+            "type": "Point"
+        },
+        "id": 13,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Strand Zuid"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.883079,
+                52.456598
+            ],
+            "type": "Point"
+        },
+        "id": 19,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Twiske"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.81287,
+                52.370687
+            ],
+            "type": "Point"
+        },
+        "id": 24,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "De Natureluur"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.851196,
+                52.369435
+            ],
+            "type": "Point"
+        },
+        "id": 30,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Mercatorplein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.873954,
+                52.387868
+            ],
+            "type": "Point"
+        },
+        "id": 33,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Talud Westergasterrein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.86766,
+                52.368074
+            ],
+            "type": "Point"
+        },
+        "id": 34,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Bellamyplein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.846022,
+                52.366268
+            ],
+            "type": "Point"
+        },
+        "id": 35,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Rembrandtpark"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.911964,
+                52.350171
+            ],
+            "type": "Point"
+        },
+        "id": 39,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Waterkant Weesperzijde"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.822354,
+                52.371645
+            ],
+            "type": "Point"
+        },
+        "id": 43,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Sloterstrand"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.813192,
+                52.333911
+            ],
+            "type": "Point"
+        },
+        "id": 46,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Nieuwe Meer"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.908303,
+                52.367782
+            ],
+            "type": "Point"
+        },
+        "id": 47,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Wertheimpark"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.885019,
+                52.359328
+            ],
+            "type": "Point"
+        },
+        "id": 90,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Rijksmuseum"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.986484,
+                52.354269
+            ],
+            "type": "Point"
+        },
+        "id": 57,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Diemerpark"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.965879,
+                52.286413
+            ],
+            "type": "Point"
+        },
+        "id": 58,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "De Hoge Dijk"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.996601,
+                52.303376
+            ],
+            "type": "Point"
+        },
+        "id": 59,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Gaasperplas"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.929492,
+                52.295017
+            ],
+            "type": "Point"
+        },
+        "id": 60,
+        "properties": {
+            "Categorie": "Stadsstranden",
+            "Naam_locatie": "Ouderkerkerplas"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.932628,
+                52.354935
+            ],
+            "type": "Point"
+        },
+        "id": 77,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Hogeweg / Linnaeusparkweg"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.881524,
+                52.36367
+            ],
+            "type": "Point"
+        },
+        "id": 78,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Americain"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.815102,
+                52.35979
+            ],
+            "type": "Point"
+        },
+        "id": 81,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Sloterplas"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.892601,
+                52.389309
+            ],
+            "type": "Point"
+        },
+        "id": 82,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Het Stenen Hoofd"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.881186,
+                52.357087
+            ],
+            "type": "Point"
+        },
+        "id": 93,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Museumplein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.868649,
+                52.365212
+            ],
+            "type": "Point"
+        },
+        "id": 94,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Ten Kateplein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.859683,
+                52.379247
+            ],
+            "type": "Point"
+        },
+        "id": 97,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Karel Doormanplein"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.890212,
+                52.389475
+            ],
+            "type": "Point"
+        },
+        "id": 98,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Barentszplein "
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.917206,
+                52.340819
+            ],
+            "type": "Point"
+        },
+        "id": 123,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Amstelkwartier"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.820171,
+                52.361928
+            ],
+            "type": "Point"
+        },
+        "id": 116,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Christoffel Plantijnpad"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.793161,
+                52.375855
+            ],
+            "type": "Point"
+        },
+        "id": 118,
+        "properties": {
+            "Categorie": "Kinderboerderijen",
+            "Naam_locatie": "De Strohalm"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.908035,
+                52.333203
+            ],
+            "type": "Point"
+        },
+        "id": 127,
+        "properties": {
+            "Categorie": "Ligweiden",
+            "Naam_locatie": "Amstelglorie"
+        },
+        "type": "Feature"
+    },
+    {
+        "geometry": {
+            "coordinates": [
+                4.9222639,
+                52.3476376
+            ],
+            "type": "Point"
+        },
+        "id": 128,
+        "properties": {
+            "Categorie": "Fonteinen",
+            "Naam_locatie": "Maliebaan"
+        },
+        "type": "Feature"
+    }
+]
+
+export default DATA;
